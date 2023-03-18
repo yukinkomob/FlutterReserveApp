b550abefb5b1196718da20f1ea8137684c820395
yukinkomob <yukinkopen@gmail.com>
2023-03-19 00:29:05 +0900

    カレンダーの日にちを選択可能にした

--- lib/calendar_page.dart
+++ lib/calendar_page.dart
@@ -1,17 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
+  const CalendarPage({super.key});
+
+  @override
+  State<StatefulWidget> createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage> {
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _selectedDay = _focusedDay;
+    // _selectedEvents = ValueNotifier(_getEventsForDay(_selectedDay!));
+  }
+
+  void _onDaySelected(DateTime selectedDay, DateTime focusedDay) {
+    if (!isSameDay(_selectedDay, selectedDay)) {
+      setState(() {
+        _selectedDay = selectedDay;
+        _focusedDay = focusedDay;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.orange,
       body: Center(
-        child: TableCalendar(
-          firstDay: DateTime.utc(2010, 10, 16),
-          lastDay: DateTime.utc(2030, 3, 14),
-          focusedDay: DateTime.now(),
-          locale: "ja_JP",
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 100,
+            ),
+            TableCalendar(
+              firstDay: DateTime.utc(2023, 01, 01),
+              lastDay: DateTime.utc(2023, 12, 31),
+              focusedDay: _focusedDay,
+              selectedDayPredicate: (day) => isSameDay(_selectedDay, day),
+              locale: "ja_JP",
+              onDaySelected: _onDaySelected,
+            )
+          ],
         ),
       ),
     );
